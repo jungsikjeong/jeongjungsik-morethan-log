ee773943c1d73c43945a9a1b08373e06bc6ab9da
JungSik Jeong <wndtlr1024@gmail.com>
2024-03-01 02:42:00 +0900

    serviceCard null

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -5,27 +5,28 @@ import styled from "@emotion/styled"
 import { Emoji } from "src/components/Emoji"
 
 const ServiceCard: React.FC = () => {
-  if (!CONFIG.projects) return null
-  return (
-    <>
-      <StyledTitle>
-        <Emoji>🌟</Emoji> Service
-      </StyledTitle>
-      <StyledWrapper>
-        {CONFIG.projects.map((project, idx) => (
-          <a
-            key={idx}
-            href={`${project.href}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiFillCodeSandboxCircle className="icon" />
-            <div className="name">{CONFIG.projects[0].name}</div>
-          </a>
-        ))}
-      </StyledWrapper>
-    </>
-  )
+  return <></>
+  // if (!CONFIG?.projects as any) return null
+  // return (
+  //   <>
+  //     <StyledTitle>
+  //       <Emoji>🌟</Emoji> Service
+  //     </StyledTitle>
+  //     <StyledWrapper>
+  //       {CONFIG.projects.map((project, idx) => (
+  //         <a
+  //           key={idx}
+  //           href={`${project.href}`}
+  //           rel="noreferrer"
+  //           target="_blank"
+  //         >
+  //           <AiFillCodeSandboxCircle className="icon" />
+  //           <div className="name">{CONFIG.projects[0].name}</div>
+  //         </a>
+  //       ))}
+  //     </StyledWrapper>
+  //   </>
+  // )
 }
 
 export default ServiceCard
